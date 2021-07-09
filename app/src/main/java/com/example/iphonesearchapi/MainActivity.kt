14a91fcd95a6a0388c092f1de056d023a66e9230
kushagra <kushagramadaan31@gmail.com>
2021-07-10 03:23:47 +0530

    Retrofit Commit

--- app/src/main/java/com/example/iphonesearchapi/MainActivity.kt
+++ app/src/main/java/com/example/iphonesearchapi/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.iphonesearchapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.LifecycleOwner
+import com.example.iphonesearchapi.network.IphoneApiService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import kotlin.math.log
+
+const val BaseUrl = "https://itunes.apple.com/"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BaseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val service = retrofit.create(IphoneApiService::class.java)
+
+        lifecycleScope.launch {
+
+            val call = service.getResult("john jackson")
+            println(call.body()?.results?.get(0)?.artistName)
+
+        }
+         }
+
+
+
+
+
 }
